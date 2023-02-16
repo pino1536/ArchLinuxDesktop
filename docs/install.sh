1b928ae733e43a74311ab8b93a51a7c999a293d8
Pino <philip.kretzschmar.93@gmail.com>
2023-02-16 13:54:56 +0100

    Add files via upload

--- docs/install.sh
+++ docs/install.sh
@@ -114,7 +114,7 @@ TimeZone(){
     subzonearray=$(find /usr/share/zoneinfo/$zone/ -type f -printf "%f\n" | sort -V)
     subzonearray=( $subzonearray )
     for i in "${subzonearray[@]}"; do
-        subzonelist+=( $( printf "%s\t\t%s" $i "=====" ) )
+        subzonelist+=( $( printf "%s\t%s" $i "" ) )
     done
     subzone=$(whiptail --title "Time Zone" --menu "Select city:" 30 70 20 "${subzonelist[@]}" 3>&1 1>&2 2>&3 )
     TimeZone="$zone/$subzone"

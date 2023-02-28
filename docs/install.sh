0fb1fb09e9fd60b4954b949f0fd4748506549dd0
Pino <philip.kretzschmar.93@gmail.com>
2023-02-28 21:19:16 +0100

    Add files via upload

--- docs/install.sh
+++ docs/install.sh
@@ -5,7 +5,7 @@ do
     disks+=(${i})
 done
 disk=$(whiptail --title "Select Disk" --menu "Select disk device:" 25 50 11 "${disks[@]}" 3>&1 1>&2 2>&3)
-
+read -p "test"
 if (whiptail --title "Disk Partition" --yesno "Wipe Disk and default Partitions?" 25 50)
 then
     (
@@ -20,9 +20,10 @@ then
     echo w
     ) | fdisk --wipe always --wipe-partitions always /dev/${disk}
     partitionefi=($(fdisk --list -o Device,Type /dev/${disk} | grep "EFI System"))
-    mkfs.fat -F 32 ${partitionefi[0]}
+    partitionefi=${partitionefi[0]}
+    mkfs.fat -F 32 $partitionefi
 fi
-
+read -p "test"
 (
     echo n
     echo  
@@ -40,24 +41,25 @@ fi
     echo 23
     echo w
 ) | fdisk --wipe-partitions always /dev/${disk}
-
-partitionefi=$(whiptail --title "EFI Partition" --inputbox "$(fdisk --list -o Device,Type /dev/${disk} | grep "/dev/")" 25 50 3>&1 1>&2 2>&3)
-read -p "$partitionefi"
+read -p "test"
+fdisk --list /dev/${disk}
+echo "Please enter the EFI Partition (Example: /dev/sda1)\n"
+read partitionefi
 partitionswap=($(fdisk --list -o Device,Type /dev/${disk} | grep "Linux swap"))
 partitionroot=($(fdisk --list -o Device,Type /dev/${disk} | grep "Linux root"))
-
+read -p "test"
 # 1.10 Format the partitions
 mkswap ${partitionswap[0]}
 mkfs.ext4 ${partitionroot[0]}
-
+read -p "test"
 # 1.11 Mount the file systems
 mount ${partitionroot[0]} /mnt
-mount --mkdir ${partitionefi[0]} /mnt/boot
+mount --mkdir ${partitionefi} /mnt/boot
 swapon ${partitionswap[0]}
-
+read -p "test"
 # 2.1 Select the mirrors
 reflector --latest 20 --protocol https --save /etc/pacman.d/mirrorlist
-
+read -p "test"
 # 2.2 Install essential packages
 cpu=$(whiptail --title "GPU" --menu "Select CPU:" 25 50 11 "AMD" "" "Intel" "" 3>&1 1>&2 2>&3)
 local microcode=""
@@ -66,10 +68,10 @@ case ${cpu} in
     "Intel") microcode="intel-ucode" ;;
 esac
 pacstrap -K /mnt base linux linux-firmware grub efibootmgr networkmanager ${microcode}
-
+read -p "test"
 # 3.1 Configure the system
 genfstab -U /mnt >> /mnt/etc/fstab
-
+read -p "test"
 # 3.3 Time zone
 local getsubzones=()
 local subzones=()
@@ -81,22 +83,22 @@ done
 subzone=$(whiptail --title "Time Zone" --menu "Select city:" 25 50 11 "${subzones[@]}" 3>&1 1>&2 2>&3)
 ln -sf /mnt/usr/share/zoneinfo/${zone}/${subzone} /mnt/etc/localtime
 arch-chroot /mnt hwclock --systohc
-
+read -p "test"
 # 3.4 Localization
 echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 keymap=$(whiptail --title "Choose Your Keyboard" --menu "Set the Keyboard Layout:" 25 50 11 "de" "German" "fr" "France" "ru" "Russia" "uk" "Unitet Kindom" "us" "USA" 3>&1 1>&2 2>&3)
 echo "KEYMAP=${keymap}" > /mnt/etc/vconsole.conf
-
+read -p "test"
 # 3.5 Network configuration
 hostname=$(whiptail --title "Hostname" --inputbox "What is your new hostname?" 25 50 3>&1 1>&2 2>&3)
 echo ${hostname} > /mnt/etc/hostname
 echo -ne "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t${hostname}.localdomain ${hostname}" > /mnt/etc/hosts
 arch-chroot /mnt systemctl enable NetworkManager.service
-
+read -p "test"
 # 3.6 Initramfs
-
+read -p "test"
 # 3.7 Root password
 rootpw=$(whiptail --title "Set new root password" --passwordbox "Please set your new root password..." 25 50 3>&1 1>&2 2>&3)
 echo -e "${rootpw}\n${rootpw}" | arch-chroot /mnt passwd
@@ -104,7 +106,8 @@ user=$(whiptail --title "Please provide sudo username" --inputbox "Please provid
 arch-chroot /mnt useradd -m -G wheel ${user}
 userpw=$(whiptail --title "Getting user password" --passwordbox "Please enter your new user's password: " 25 50 3>&1 1>&2 2>&3)
 echo -e "${userpw}\n${userpw}" | arch-chroot /mnt passwd ${user}
-
+read -p "test"
 # 3.8 Boot loader
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=boot --bootloader-id=GRUB
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+read -p "test"
